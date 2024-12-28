bc2ca484d4106497ae5832de5a7653862fa46194
Ashish5692 <ashishyadav5692@gmail.com>
2024-12-29 01:11:46 +0530

    Created Register Page

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import ProductController from "./src/controllers/product.controller.js";
+import UserController from "./src/controllers/user.controller.js";
 import ejsLayouts from "express-ejs-layouts";
 import path from "path";
 import validationMiddlewares from "./src/middlewares/validation.middlewares.js";
 import { uploadFile } from "./src/middlewares/file-upload.middleware.js";
+
 
 const server = express();
 
@@ -18,8 +20,12 @@ server.set("views", path.join(path.resolve(), "src", "views"));
 
 server.use(ejsLayouts); // we will use this layout for our views
 
-//create instance of ProductController
+//create instance of ProductController beacuse method need to call using instances
 const productController = new ProductController();
+
+const usersController = new UserController();
+
+server.get('/register', usersController.getRegister);
 server.get("/", productController.getProducts);
 server.get("/new", productController.getAddForm);
 server.get("/update-product/:id", productController.getUpdateProductView); //id is URL parameter
